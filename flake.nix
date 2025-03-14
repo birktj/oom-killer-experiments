5cfca086df8760574a912530ca926af620d4892a
Birk Tjelmeland <git@birktj.no>
2025-03-14 18:38:50 +0100

    Some updates

--- flake.nix
+++ flake.nix
@@ -2,32 +2,51 @@
     inputs.nixpkgs.url = "nixpkgs";
 
     outputs = { nixpkgs, self}: {
-        packages.x86_64-linux.test-base = self.nixosConfigurations.test-1.config.system.build.vm;
+        packages.x86_64-linux = builtins.mapAttrs (name: value: value.config.system.build.vm) self.nixosConfigurations;
+
         nixosConfigurations.test-base = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
-                "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
                 self.nixosModules.vm-basics
-                self.nixosModules.mem-stress-test
             ];
         };
 
-        packages.x86_64-linux.test-zram = self.nixosConfigurations.test-1.config.system.build.vm;
+        nixosConfigurations.test-base-graphics = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                self.nixosModules.vm-basics
+                self.nixosModules.minimal-graphic
+            ];
+        };
+
+        nixosConfigurations.test-memlimit = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                self.nixosModules.vm-basics
+                self.nixosModules.mem-limit
+            ];
+        };
+
         nixosConfigurations.test-zram = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
-                "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
                 self.nixosModules.vm-basics
-                self.nixosModules.mem-stress-test
             ];
         };
 
-        nixosModules.vm-basics = {pkgs, lib, ...}: {
-            virtualisation.graphics = false;
-            virtualisation.diskImage = null;
-            # virtualisation.fileSystems = lib.mkForce { };
-            virtualisation.cores = 8;
+        nixosModules.vm-basics = {pkgs, lib, modulesPath, ...}: {
+            imports = [
+                "${modulesPath}/virtualisation/qemu-vm.nix"
+                self.nixosModules.mem-test-tools
+            ];
 
+            virtualisation.graphics = lib.mkDefault false;
+            virtualisation.diskImage = null;
+            virtualisation.cores = lib.mkDefault 8;
+            virtualisation.memorySize = lib.mkDefault 1024;
+
+            # Somehow we end up with "/" using mode=777 when using tmpfs.
+            # But then sshd won't let us log in
             virtualisation.fileSystems."/" = {
                 options = lib.mkForce [ "mode=755" "uid=0" "gid=0" ];
             };
@@ -42,6 +61,7 @@
                 password = "";
                 openssh.authorizedKeys.keys = [
                     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrvdnfRte2aM39d+GdUVt+KI6HqP8opmmuxYXKdBMzF birk@erebus"
+                    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuZp5Quo2ubCkxqX6D1DIqKVf+p98ffcNg6f9M6Nc9X birk@granite"
                 ];
             };
 
@@ -51,7 +71,6 @@
 
             virtualisation.forwardPorts = [
                 { from = "host"; host.port = 2222; guest.port = 22; }
-                { from = "host"; host.port = 2221; guest.port = 2221; }
             ];
             networking.firewall.enable = false;
             services.openssh.enable = true;
@@ -61,9 +80,6 @@
                 htop
                 tmux
                 xterm
-                cargo
-                rustc
-                gcc
             ];
 
             environment.sessionVariables.TERM = "xterm-256color";
@@ -71,8 +87,29 @@
             system.stateVersion = "24.11";
         };
 
-        nixosModules.mem-stress-test = {pkgs, ...}: {
-            environment.systemPackages = [(pkgs.writers.writePython3Bin "mem-stress-test" {} ''
+        nixosModules.mem-limit = {pkgs, ...}: {
+            systemd.slices."user".sliceConfig = {
+                MemoryMax="600M";
+            };
+        };
+
+        nixosModules.minimal-graphic = {pkgs, ...}: {
+            virtualisation.graphics = true;
+            virtualisation.memorySize = 2 * 1024;
+
+            services.xserver.enable = true;
+            services.displayManager.autoLogin = {
+                enable = true;
+                user = "test";
+            };
+            services.xserver.windowManager.openbox.enable = true;
+            environment.systemPackages = with pkgs; [
+                firefox
+            ];
+        };
+
+        nixosModules.mem-test-tools = {pkgs, ...}: let
+            mem-stress-test = pkgs.writers.writePython3Bin "mem-stress-test" {} ''
                 import time
                 import sys
 
@@ -89,7 +126,20 @@
                         s = ' ' * pos + "#" + ' ' * (20 - pos)
                         print(s, end="\r")
                         time.sleep(1)
-            '')];
+            '';
+
+            mem-ripgrep-test = pkgs.writeShellScriptBin "mem-ripgrep-test" ''
+                cargo install ripgrep
+            '';
+        in {
+            environment.systemPackages = with pkgs; [
+                mem-stress-test
+                mem-ripgrep-test
+                cargo
+                rustc
+                gcc
+                fish
+            ];
         };
     };
 }
